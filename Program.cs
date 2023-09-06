76a1d8703da6d5c66d415f893161258c113e9344
Elias <Elias@DESKTOP-B1NE5G0>
2023-09-06 15:50:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloEdugrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var name = Console.ReadLine(); //InmatningsHanterare, läser vad som skrivs in i konsollen
+            var currentDate = DateTime.Now;
+            Console.WriteLine(value: $"Hej, {name}, på {currentDate}");
+        }
+    }
+}
